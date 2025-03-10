29b61b76813466d719ee6a324433c018a1b3e991
code1man <149554589+code1man@users.noreply.github.com>
2025-03-10 22:14:56 +0800

    init Admit

--- src/main/java/org/csu/demo/service/BusinessService.java
+++ src/main/java/org/csu/demo/service/BusinessService.java
@@ -15,26 +15,31 @@ public class BusinessService {
     @Autowired
     private ItemDao itemDao;
 
+    /*获取所有商品的信息*/
     public List<Item> findAll() {
         return businessDao.getAllBusinessItems();
     }
 
+    /*插入新的商品*/
     public int insertItem(Item item) {
         int tmp1 = businessDao.insertBusinessItem(item);
         int tmp2 = itemDao.insertItem(item);
         return (tmp1 + tmp2) - 1;
     }
 
+    /*删除商品*/
     public int deleteItem(int itemId) {
         int tmp1 = businessDao.deleteBusinessItem(itemId);
         int tmp2 = itemDao.deleteItem(itemId);
         return (tmp1 + tmp2) - 1;
     }
 
+    /*更新商品信息*/
     public int updateItem(Item item) {
         return businessDao.updateBusinessItem(item);
     }
 
+    /*查询剩余商品数量*/
     public int getItemCount(int itemId) {
         return businessDao.queryBusinessItemCount(itemId);
     }
