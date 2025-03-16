8d39d76964ffa5dadf59147c30b065ac753702ff
code1man <149554589+code1man@users.noreply.github.com>
2025-03-16 14:45:52 +0800

    修改一些bug

--- src/test/java/org/csu/demo/NaiLongApplicationTests.java
+++ src/test/java/org/csu/demo/NaiLongApplicationTests.java
@@ -13,11 +13,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 @SpringBootTest
 @MapperScan("org.csu.demo.persistence")
+@ActiveProfiles("test")  // 启用 test 这个 Profile
 class NaiLongApplicationTests {
 
     @Autowired
--- src/test/java/org/csu/demo/OrderTest.java
+++ src/test/java/org/csu/demo/OrderTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @MapperScan("org.csu.demo.mappers")
+@ActiveProfiles("test")  // 启用 test 这个 Profile
 public class OrderTest {
     @Autowired
     private UserMapper userMapper;
